6a8d3636ccd9e199cacab9cdbfdc874a7dd4c87b
HeadcrabJ <reymond315qq@gmail.com>
2022-10-08 05:35:21 -0700

    Add tooltip to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,16 @@
 ﻿import React from 'react'
-import { Typography } from '@mui/material'
+import { Tooltip, Typography } from '@mui/material'
 
 function Footer () {
   return (
-    <Typography
-      color={'text.secondary'}
-      sx={{ textAlign: 'center', marginBottom: 1 }}
-    >
-      © {new Date().getFullYear()} {window.location.hostname}
-    </Typography>
+    <Tooltip title={'Developed by Danil'}>
+      <Typography
+        color={'text.secondary'}
+        sx={{ textAlign: 'center', marginBottom: 1 }}
+      >
+        © {new Date().getFullYear()} {window.location.hostname}
+      </Typography>
+    </Tooltip>
   )
 }
 
