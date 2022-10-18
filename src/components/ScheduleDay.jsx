1966da3fa927a681482cd441e2f556660cce7806
HeadcrabJ <reymond315qq@gmail.com>
2022-10-18 09:05:40 -0700

    Decrease title size in table Small fixes and improvements

--- src/components/ScheduleDay.jsx
+++ src/components/ScheduleDay.jsx
@@ -14,41 +14,40 @@ import PropTypes from 'prop-types'
 
 function ScheduleDay ({ table, date }) {
   const isToday = table.getElementsByTagName('table')[0].style.color
-  const prop = { paddingTop: '2px', paddingBottom: '2px' }
 
   return (
     <Paper
       id={isToday ? 'scrollHere' : ''}
       sx={isToday ? { bgcolor: 'action.hover' } : {}}
     >
-      <Toolbar>
+      <Toolbar variant={'dense'}>
         <Typography>
-          {date} {isToday ? ' (Сегодня)' : ''}
+          {date + (isToday ? ' (Сегодня)' : '')}
         </Typography>
       </Toolbar>
-      <TableContainer sx={{ marginBottom: 1 }}>
+      <TableContainer sx={{ marginBottom: 1, 'th, td': { paddingTop: '2px', paddingBottom: '2px' } }}>
         <Table size={'small'}>
           <TableHead>
             <TableRow>
-              <TableCell sx={prop} align={'center'}>№</TableCell>
-              <TableCell sx={prop} align={'left'}>предмет</TableCell>
-              <TableCell sx={prop} align={'right'}>каб</TableCell>
-              <TableCell sx={prop} align={'right'}>преподаватель</TableCell>
-              <TableCell sx={prop} align={'right'}>время</TableCell>
+              <TableCell align={'center'}>№</TableCell>
+              <TableCell align={'left'}>предмет</TableCell>
+              <TableCell align={'right'}>каб</TableCell>
+              <TableCell align={'right'}>преподаватель</TableCell>
+              <TableCell align={'right'}>время</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {[1, 2, 3, 4, 5].map((tableRow, rowIndex) => (
+            {[1, 2, 3, 4, 5].map((tableRow) => (
               <TableRow
-                key={rowIndex}
+                key={tableRow}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 {[table.getElementsByClassName(`time_background${tableRow}`)[0]].map((lesson, lessonIndex) => (
                   <React.Fragment key={lessonIndex}>
-                    <TableCell sx={prop} align={'center'}>
+                    <TableCell align={'center'}>
                       {lesson.getElementsByTagName('td')[0].innerText}
                     </TableCell>
-                    <TableCell sx={prop} align={'left'} style={{ whiteSpace: 'pre-wrap' }}>
+                    <TableCell align={'left'} style={{ whiteSpace: 'pre-wrap' }}>
                       {(() => {
                         const item = lesson.getElementsByTagName('td')[1]
                         const link = item.getElementsByTagName('span')[0]
@@ -62,13 +61,13 @@ function ScheduleDay ({ table, date }) {
                         }
                       })()}
                     </TableCell>
-                    <TableCell sx={prop} align={'right'} style={{ whiteSpace: 'pre-wrap' }}>
+                    <TableCell align={'right'} style={{ whiteSpace: 'pre-wrap' }}>
                       {lesson.getElementsByTagName('td')[2].innerHTML.replace('<br>', '\n')}
                     </TableCell>
-                    <TableCell sx={prop} align={'right'} style={{ whiteSpace: 'pre-wrap' }}>
+                    <TableCell align={'right'} style={{ whiteSpace: 'pre-wrap' }}>
                       {lesson.getElementsByTagName('td')[3].innerHTML.replace('<br>', '\n')}
                     </TableCell>
-                    <TableCell sx={prop} align={'right'} style={{ whiteSpace: 'nowrap' }}>
+                    <TableCell align={'right'} style={{ whiteSpace: 'nowrap' }}>
                       {lesson.getElementsByTagName('td')[4].innerText}
                       <br />
                       {table.getElementsByClassName(`time_background${tableRow}`)[1].getElementsByTagName('td')[0].innerText}
