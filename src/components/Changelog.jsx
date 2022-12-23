b21acf7d09f11dd946da098349291eca0d7dc443
HeadcrabJ <reymond315qq@gmail.com>
2022-12-24 00:08:11 +0500

    Add changelog date

--- src/components/Changelog.jsx
+++ src/components/Changelog.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Card, CardContent, Typography } from '@mui/material'
 import PropTypes from 'prop-types'
 
-function Changelog ({ v, children }) {
+function Changelog ({ v, date, children }) {
   return (
     <Card variant="outlined" sx={{ maxWidth: 600, mb: '8px' }}>
       <CardContent sx={{ '&:last-child': { pb: '16px' } }}>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {date}
+        </Typography>
         <Typography variant="h5" component="div">
           Version {v}
         </Typography>
@@ -19,6 +22,7 @@ function Changelog ({ v, children }) {
 
 Changelog.propTypes = {
   v: PropTypes.string,
+  date: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,36 +19,41 @@ function HomePage () {
       </Typography>
 
       <Container>
-        <Changelog v={'2.7'}>
-          <Item>Добавлен чейнджлог</Item>
+        <Changelog v={'2.8'} date={'24 december 2022'}>
+          <Item>Добавлена дата изменения в чейнджлоге</Item>
         </Changelog>
 
-        <Changelog v={'2.6'}>
+        <Changelog v={'2.7'} date={'23 december 2022'}>
+          <Item>Добавлен чейнджлог</Item>
+          <Item>Изменен дизайн карточек групп и преподавателей</Item>
+        </Changelog>
+
+        <Changelog v={'2.6'} date={'23 december 2022'}>
           <Item>Исправлена ошибка из-за которой не обновляется контент расписания</Item>
         </Changelog>
 
-        <Changelog v={'2.5'}>
+        <Changelog v={'2.5'} date={'21 december 2022'}>
           <Item>Исправлена ширина полосы загрузки на экранах выше 1100 пикселей</Item>
         </Changelog>
 
-        <Changelog v={'2.4'}>
+        <Changelog v={'2.4'} date={'18 december 2022'}>
           <Item>Добавлено диалоговое окно переезда на новый домен</Item>
         </Changelog>
 
-        <Changelog v={'2.3'}>
+        <Changelog v={'2.3'} date={'18 december 2022'}>
           <Item>Удалены неиспользуемые утилиты</Item>
           <Item>Изменена полоса загрузки</Item>
         </Changelog>
 
-        <Changelog v={'2.2'}>
+        <Changelog v={'2.2'} date={'17 december 2022'}>
           <Item>Добавлена надпись Developed by под заголовком</Item>
         </Changelog>
 
-        <Changelog v={'2.1'}>
+        <Changelog v={'2.1'} date={'16 december 2022'}>
           <Item>Добавлены иконки в меню навигации</Item>
         </Changelog>
 
-        <Changelog v={'2.0'}>
+        <Changelog v={'2.0'} date={'16 december 2022'}>
           <Item>Добавлено нижнее меню навигации вместо шапки</Item>
         </Changelog>
       </Container>
