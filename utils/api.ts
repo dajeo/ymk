b895d1d38379b434ba86016f4b55b3ed50795e13
Headcrab <headcrabj1@gmail.com>
2023-02-01 12:20:37 +0500

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -20,7 +20,7 @@ function useGroups(department: string) {
   return { groups: data, isError: error, isLoading };
 }
 
-function useSchedule(department?: string, group?: string, week?: number) {
+function useSchedule(department: string | string[], group: string | string[], week: number) {
   const { data, error, isLoading } = useSWR(`/api/students/${department}/${group}/${week}`, fetcher, config);
   return { schedule: data, isError: error, isLoading };
 }
