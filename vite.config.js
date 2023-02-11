4562bc1a8c74d4dc3f3bb6ac570f480dc33a3261
User3 <headcrabj1@gmail.com>
2023-02-11 21:42:38 +0500

    Update dependencies Remove PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 ﻿import { defineConfig } from "vite";
 import React from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    React(),
-    VitePWA({
-      registerType: "autoUpdate"
-    })
+    React()
   ]
 });
